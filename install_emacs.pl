0b34f60c818d064e8c144af575425d18a4929548
xiaonan song <4king-gruuby@163.com>
2013-11-15 13:49:30 +0800

    a test

--- install_emacs.pl
+++ install_emacs.pl
@@ -12,8 +12,6 @@ my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my $day = ($year-100).($mon+1).$mday.$hour.$min.$sec;
 
 move("$ENV{HOME}/.emacs", "/tmp/emacs_config_backup/.emacs$day");
-
-#move(".emacs", "$ENV{HOME}/.emacs");
 copy("./.emacs", "$ENV{HOME}/.emacs");
 
 
